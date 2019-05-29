410f21f97c894123a9d85bf243e06cfe3e347bc9
Eric Rasmussen <ericrasmussen@gmail.com>
2019-05-29 13:49:14 -0700

    sample solution to ex1

--- src/exercise1.rs
+++ src/exercise1.rs
@@ -1,7 +1,22 @@
 pub fn find_largest_element(elems: &Vec<i64>) -> Option<i64> {
 
-    // your code here
+     // if the given `Vec` is empty we won't have anything to return, so our
+    // return value is an optional type.  since it can be mutated by the inner
+    // loop, we specify that it's mutable.
+    let mut max: Option<i64> = None;
 
+    // this converts our `Vec<i64>` into an iterator that
+    // we can loop through.
+    for e in elems {
+        // gets the current `Some(val)` from `max`, or defaults to 0
+        // to 0
+        if e > &max.unwrap_or(0) {
+            max = Some(*e);
+        }
+    }
+
+    // ending without a semi-colon tells rust we're returning this as a value
+    max
 }
 
 
